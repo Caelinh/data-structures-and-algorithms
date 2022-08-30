dc6a1ee1bf03f5b35ee105508b7165ec37565b6d
Caelinh <Caelinhargrave@gmail.com>
2022-08-29 23:49:04 -0500

    Completed Linked list and node classes

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -5,10 +5,30 @@ package datastructures.linkedlist;
 
 public class LinkedList {
   Node head = null;
-  Node tail = null;
 
-  public LinkedList(Node head) {
-    this.head = head;
+  public void insert(int value){
+    Node newHead = new Node(value);
+    newHead.next = head;
+    head = newHead;
+  }
+
+  public boolean includes(int value){
+    Node current = head;
+    while(current.next != null){
+      if(current.next.value == value){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String toString(){
+    Node current = head;
+    String linkedlistString = "";
+    while (current.next != null){
+      linkedlistString += ("{"+current.value+"} -->");
+    } return linkedlistString + "NULL";
+
   }
 
 
